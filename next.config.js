424b30d685c1d7dde40dbbf554f885c778417355
Ammar2123 <ammar@nagarji.in>
2026-03-16 15:37:34 +0530

    fix: remove webpack config for next16

--- next.config.js
+++ next.config.js
@@ -23,10 +23,6 @@ const nextConfig = {
 
   turbopack: {},
 
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), { canvas: "canvas" }];
-    return config;
-  },
 };
 
 module.exports = nextConfig;
